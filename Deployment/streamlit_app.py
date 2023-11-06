f5d031f95b7f0f5b0fd095c9975e64af9b8f2551
Peter Wainaina <80960028+wainaina-peter@users.noreply.github.com>
2023-11-06 21:38:21 +0300

    Add files via upload

---
+++ Deployment/streamlit_app.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+import streamlit as st
+import os
+import string
+model = pickle.load(open('knn.pkl', 'rb'))
+
+def main():
+    st.title('THeart Failure Prediction Model')
+
+    #input variables
+    Age = st.text_input('Input your age')
+    Sex = st.text_input('Input your sex')
+    ChestPainType = st.text_input('What is the Type of chest pains you are experiencing?')
+    RestingBP = st.text_input('What is your BP rate while resting?')
+    Cholesterol = st.text_input('What is your cholesterol level?')
+    FastingBS = st.text_input('What is your BS when fasting?')
+    RestingECG = st.text_input('What is your ECG when resting?')
+    MaxHR = st.text_input('What is your max HR?')
+    ExerciseAngina = st.text_input('Your Exercise Angina')
+    Oldpeak = st.text_input('What is your Oldpeak')
+    ST_Slope = st.text_input('Input your ST_Slope')
+    makeprediction = ""
+
+
+    #prediction code
+    if st.button('Predict'):
+        makeprediction = model.predict([[Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope]])
+        output = round(makeprediction[0],2)
+        st.success('The chance of getting heart failure {}'.format(output))
+
+if __name__=="__main__":
+    main()        
+
