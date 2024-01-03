dd282c54f2163a2b7f228f37078a2aed02234dab
Peter Wainaina <80960028+wainaina-peter@users.noreply.github.com>
2024-01-03 22:15:37 +0300

    Update streamlit_app.py

--- Deployment/streamlit_app.py
+++ Deployment/streamlit_app.py
@@ -1,34 +1,80 @@
-import pickle
-import numpy as np
 import streamlit as st
-import os
-import string
-model = pickle.load(open('heartfailuremodel.pkl', 'rb'))
+import numpy as np
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+
+# Define the encoders and scaler
+enc_oe = OrdinalEncoder()
+enc_ohe = OneHotEncoder()
+scaler = StandardScaler()
+
+def make_prediction_knn_preprocessed(model, input_data, enc_oe, enc_ohe, scaler):
+    import pandas as pd
+    
+    # Create a DataFrame from the input data
+    input_df = pd.DataFrame([input_data])
+
+    # Define the columns used during training
+    training_columns = ['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol', 'FastingBS', 'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']
+
+    # Transform categorical columns using OrdinalEncoder
+    binary_var = ['Sex', 'FastingBS', 'ExerciseAngina', 'RestingECG']
+    enc_oe.fit(input_df[binary_var])
+    input_df[binary_var] = enc_oe.transform(input_df[binary_var])
+
+    # Transform 'ChestPainType' and 'ST_Slope' using OneHotEncoder
+    multi_categ = ['ChestPainType', 'ST_Slope']
+    transformed = pd.DataFrame(enc_ohe.transform(input_df[multi_categ]).toarray(), columns=enc_ohe.get_feature_names_out(multi_categ))
+    input_df = pd.concat([input_df, transformed], axis=1).drop(multi_categ, axis=1)
+
+    # Define the numeric variables
+    numeric_vars = ['Age', 'RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak']
+
+    # Scale the numeric variables
+    input_df[numeric_vars] = scaler.transform(input_df[numeric_vars])
+
+    # Make a prediction using the preprocessed input data and the KNN classifier
+    prediction = model.predict(input_df)
+
+    return prediction
 
 def main():
-    st.title('Heart Failure Prediction Model')
+    st.title('Heart Disease Prediction')
 
-    #input variables
-    Age = st.text_input('Input your age')
-    Sex = st.text_input('Input your sex')
-    ChestPainType = st.text_input('What is the Type of chest pains you are experiencing?')
-    RestingBP = st.text_input('What is your BP rate while resting?')
-    Cholesterol = st.text_input('What is your cholesterol level?')
-    FastingBS = st.text_input('What is your BS when fasting?')
-    RestingECG = st.text_input('What is your ECG when resting?')
-    MaxHR = st.text_input('What is your max HR?')
-    ExerciseAngina = st.text_input('Your Exercise Angina')
-    Oldpeak = st.text_input('What is your Oldpeak')
-    ST_Slope = st.text_input('Input your ST_Slope')
+    # Input widgets for user input
+    Age = st.number_input('Enter your age', min_value=0, max_value=120, value=25)
+    Sex = st.selectbox('Select your gender', ['Male', 'Female'])
+    ChestPainType = st.selectbox('Select your chest pain type', ['Typical Angina', 'Atypical Angina', 'Non-anginal Pain', 'Asymptomatic'])
+    RestingBP = st.number_input('Enter your resting blood pressure (mm Hg)', min_value=0, value=120)
+    Cholesterol = st.number_input('Enter your cholesterol level (mg/dl)', min_value=0, value=200)
+    FastingBS = st.selectbox('Is your fasting blood sugar > 120 mg/dl?', ['Yes', 'No'])
+    RestingECG = st.selectbox('What is your resting electrocardiographic results?', ['Normal', 'ST-T wave abnormality', 'Left ventricular hypertrophy'])
+    MaxHR = st.number_input('Enter your maximum heart rate', min_value=0, value=150)
+    ExerciseAngina = st.selectbox('Do you experience exercise-induced angina?', ['Yes', 'No'])
+    Oldpeak = st.number_input('Enter your ST depression induced by exercise relative to rest', value=0.0)
+    ST_Slope = st.selectbox('Select your slope of the peak exercise ST segment', ['Upsloping', 'Flat', 'Downsloping'])
+
     makeprediction = ""
 
-
-    #prediction code
+    # Prediction code
     if st.button('Predict'):
-        makeprediction = model.predict([[Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope]])
-        output = round(makeprediction[0],2)
-        st.success('The chance of getting heart failure {}'.format(output))
+        # Preprocess the input data
+        input_data = {
+            'Age': Age,
+            'Sex': Sex,
+            'ChestPainType': ChestPainType,
+            'RestingBP': RestingBP,
+            'Cholesterol': Cholesterol,
+            'FastingBS': FastingBS,
+            'RestingECG': RestingECG,
+            'MaxHR': MaxHR,
+            'ExerciseAngina': ExerciseAngina,
+            'Oldpeak': Oldpeak,
+            'ST_Slope': ST_Slope
+        }
 
-if __name__=="__main__":
-    main()        
+        # Make predictions
+        predicted_heart_disease_knn_preprocessed = make_prediction_knn_preprocessed(classifier_KNN, input_data, enc_oe, enc_ohe, scaler)
+        st.success('The predicted result is {}'.format(predicted_heart_disease_knn_preprocessed))
 
+if __name__ == "__main__":
+    main()
